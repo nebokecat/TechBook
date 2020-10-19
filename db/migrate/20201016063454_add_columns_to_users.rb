5aa1357fd85899572bcadbfc145fe14931f956b1
EC2 Default User <ec2-user@ip-172-31-34-227.us-west-2.compute.internal>
2020-10-19 08:20:10 +0000

    Ralease to heroku

---
+++ db/migrate/20201016063454_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :location, :string
+    add_column :users, :image, :string
+  end
+end
