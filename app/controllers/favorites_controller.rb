da7138acdd621af9e82fa992838b8ab4d368cd54
EC2 Default User <ec2-user@ip-172-31-34-227.us-west-2.compute.internal>
2020-10-24 14:16:08 +0000

    add ajax favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,16 +4,13 @@ class FavoritesController < ApplicationController
     
      # お気に入り登録
   def create
-    if @book.user_id != current_user.id   # 投稿者本人以外に限定
-      @favorite = Favorite.create(user_id: current_user.id, book_id: @book.id)
-    end
-    redirect_to books_path
+      favorite = current_user.favorites.build(book_id: @book.id)
+      favorite.save
   end
   # お気に入り削除
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
-    @favorite.destroy
-    redirect_to books_path
+    favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
+    favorite.destroy
   end
 
   private
